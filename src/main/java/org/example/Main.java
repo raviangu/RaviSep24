b5492ce6a67a7acccfe48f6a971232ed6e765cf7
Ravikumar <ravikumarram@gmail.com>
2024-09-02 15:48:33 +0530

    Intro to OOPS

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.oops_1.Student;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name="Ravi";
+        s1.age=42;
+        s1.psp=98;
+        s1.joinClass();
+
+    }
+}
---
+++ src/main/java/org/example/oops_1/Student.java
@@ -0,0 +1,18 @@
+package org.example.oops_1;
+
+public class Student {
+   public int age;
+    public float psp;
+    public String name;
+
+    void changeBatch()
+    {
+        System.out.println("changing Batch");
+    }
+
+  public  void joinClass()
+    {
+        System.out.println(name + " is joining class");
+    }
+
+}
